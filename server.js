2e7fc9bffe2a01ad04ac9210732715c2bc85d774
Nihar Biswal <a.b@gmail.com>
2016-06-03 16:22:24 +1000

    final q parameter..

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/', function(req, res){
     res.send('Todo API Root');
 });
 
-app.get('/todos', function(req, res){
-    res.json(todos);
-});
+//app.get('/todos', function(req, res){
+//    res.json(todos);
+//});
 
 
 // GET /todos/:id
@@ -33,6 +33,32 @@ app.get('/todos/:id', function (req,res){
     }
 
 });
+
+
+// GET /todos?completed=true&q=dog
+
+app.get('/todos', function (req,res){
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    //debugger;
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(todos, {"completed" : true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(todos, {"completed" : false});
+    }
+
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ) {
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+            return todo.description.toLocaleLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+    }
+
+    res.json(filteredTodos);
+
+
+});
+
 
 // POST /todos
 
